48bbf982ca6610e502f32ece5e94d823d31ea1cd
Jan Wittstock <Jan.Wittstock@mailbox.tu-dresden.de>
2024-04-10 12:31:07 +0200

    implement Lorentz function

--- view Data.py
+++ view Data.py
@@ -45,53 +45,80 @@ pos_cr = [-0.255, -0.156, -0.028, 0.033, 0.25] #cm
 x0 = pos_cr
 w_cr = [123.9, 20.4, 112.6, 81.6, 121.8] #microm
 
-x_prediction = np.linspace(-0.36, 0.36, 7200)
+x_prediction = np.linspace(-0.38, 0.38, 7600)
+x_start = x_prediction[0]
 
-gamma = 0.01621281
-a = 0.80975164
+gamma = 1.65128426e-02
+a = 9.16119657e-01
+b = 1.48343394e-04
+c = 1.11589189e+00
 
-def cauchy(x, gamma, a, x0, w_cr):
-    return w_cr*(gamma / (np.pi * a * (gamma**2 + (x-x0)**2)) )  #virtuelle Daten mit freien Parametern
+def cauchy(x, gamma, a, b, c, x0, w_cr, pos_cr, limit):
+    y_predicted = w_cr * (gamma / (np.pi * a * (gamma ** 2 + (x-x0) ** 2)))  # virtuelle Daten mit freien Parametern
+    x_list = x.tolist()
 
-y = [0] * 7200
+    gesuchter_wert = (pos_cr-limit)
+    nächstes_element = min(x_list, key=lambda x: abs(x - gesuchter_wert))
+
+    index_inf_l = x_list.index(nächstes_element)
+
+    y_predicted = y_predicted - y_predicted[index_inf_l]
+    y_predicted = (b * w_cr + c) * y_predicted
+    return y_predicted
+
+
+def lorentz(x, gamma, s, alpha, x0, w_cr, pos_cr, limit):
+    y_predicted = w_cr*gamma / (1+((x-x0)/s)**2)**alpha  # virtuelle Daten mit freien Parametern
+    x_list = x.tolist()
+
+    gesuchter_wert = (pos_cr - limit)
+    nächstes_element = min(x_list, key=lambda x: abs(x - gesuchter_wert))
+
+    index_inf_l = x_list.index(nächstes_element)
+
+    y_predicted = y_predicted - y_predicted[index_inf_l]
+    #y_predicted = (b * w_cr + c) * y_predicted
+    return y_predicted
+
+y = [0] * 7600
 y_prediction = np.array(y)
 data = [x_prediction,y_prediction]
+#limit for Einflusslänge of crack (influence lenght?)
+limit = 0.12
 
-'''
+"""
 for i in range(len(pos_cr)):
-    x_single_crack = np.linspace(-0.1+pos_cr[i],0.1+pos_cr[i],2000)
-    y_single_crack = cauchy(x_single_crack,gamma,a,w_cr[i])
-    data_single_crack = [x_single_crack,y_single_crack]
-    for j,k in data_single_crack.items():
-        if k in data:
-            data_single_crack[k] += k
-'''
-def moving_average1(data, window_size):
-    return np.convolve(data, np.ones(window_size)/window_size, mode='same')
-
-
-# Fenstergröße für den gleitenden Durchschnitt
-window_size = 30
-
-
-for i in range(len(pos_cr)):
-    y_single_crack = cauchy(x_prediction, gamma, a, x0[i], w_cr[i])
-
-    limit = 0.15
+    y_single_crack = cauchy(x_prediction, gamma, a, b, c, x0[i], w_cr[i], pos_cr[i], limit)
 
     mask1 = x_prediction > (pos_cr[i]+limit)
     mask2 = x_prediction < (pos_cr[i]-limit)
     y_single_crack[mask1] = 0
     y_single_crack[mask2] = 0
 
-    y_single_crack = moving_average1(y_single_crack,window_size)
-
     plt.plot(x_prediction,y_single_crack,c='m')
     y_prediction = y_prediction+y_single_crack
 
 plt.plot(x_prediction,y_prediction,c='r')
 plt.show()
-print('last statement')
+"""
+gamma = 24.6
+s = 0.01872447
+alpha = 1.25446139
+
+for i in range(len(pos_cr)):
+    y_single_crack = lorentz(x_prediction, gamma, s, alpha, x0[i], w_cr[i], pos_cr[i], limit)
+
+    mask1 = x_prediction > (pos_cr[i]+limit)
+    mask2 = x_prediction < (pos_cr[i]-limit)
+    y_single_crack[mask1] = 0
+    y_single_crack[mask2] = 0
+
+    plt.plot(x_prediction,y_single_crack,c='m')
+    y_prediction = y_prediction+y_single_crack
+
+#plt.scatter(x_DFOS[4],y_DFOS[4])
+plt.plot(x_prediction,y_prediction,c='r')
+plt.show()
 
 
 
